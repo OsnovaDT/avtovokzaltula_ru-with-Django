e7c0be2b45902bdd5d31ef2dda15402e88439b1c
Evgeny Stepanov <maestrojane@gmail.com>
2021-07-24 15:06:32 +0300

    Add tests for Flight model

--- bus_stations/tests/test_models.py
+++ bus_stations/tests/test_models.py
@@ -3,7 +3,8 @@
 from django.test import TestCase
 
 from bus_stations.models import (
-    BusStation, Route,
+    BusStation, Route, Flight, Bus,
+    Driver, Ticket
 )
 
 
@@ -230,6 +231,134 @@ class RouteTests(TestCase):
         """Test ordering of Route model"""
 
         real_model_ordering = Route._meta.ordering
+
+        self.assertEqual(
+            real_model_ordering,
+            self.model_ordering
+        )
+
+
+class FlightTests(TestCase):
+    """Test class for Flight model"""
+
+    def setUp(self):
+        test_bus_station = BusStation.objects.create(
+            name='Автовокзал №1',
+            office_hours='10:00 - 22:00',
+            address=f'г. Тула, ул. Такая-то, дом №1',
+            phone_number=f'8-666-666-69-69'
+        )
+
+        # test_route = Route.objects.create(
+        #     name=f'Маршрут №1',
+        #     regularity='Пн;Ср',
+        #     departure_time='10:00; 22:00',
+        #     price=300,
+        #     bus_station=test_bus_station
+        # )
+
+        test_driver = Driver.objects.create(
+            passport_number='1234 12345678',
+            name='Семён',
+            second_name='Семёнов',
+            middle_name='Семёнович',
+            phone_number=89206666996,
+            age=50
+        )
+
+        test_bus = Bus.objects.create(
+            registration_number='Е666КХ',
+            mark='Ford',
+            amount_of_places='40',
+            driver=test_driver
+        )
+
+        for test_instance_index in range(TEST_INSTANCES_AMOUNT):
+            Route.objects.create(
+                id=test_instance_index,
+                name=f'Маршрут №1',
+                regularity='Пн;Ср',
+                departure_time='10:00; 22:00',
+                price=300,
+                bus_station=test_bus_station
+            )
+            Flight.objects.create(
+                route=Route.objects.get(id=test_instance_index),
+                departure_time='10:00',
+                arrival_time='22:00',
+                amount_of_free_places=30,
+                bus=test_bus
+            )
+
+        # Correct data for Flight model
+
+        self.fields_and_verbose_names = {
+            'route': 'Маршрут',
+            'departure_time': 'Время отправления',
+            'arrival_time': 'Время прибытия',
+            'amount_of_free_places': 'Число свободных мест',
+            'bus': 'Автобус'
+        }
+
+        self.fields_and_default_values = {
+            'amount_of_free_places': 30,
+        }
+
+        self.model_verbose_name = 'Рейс'
+        self.model_verbose_name_plural = 'Рейсы'
+        self.model_ordering = ['departure_time']
+        self.unique_together = ['route', 'departure_time']
+
+    def test_verbose_names(self):
+        """Test verbose_name parameter for fields of Flight instances"""
+
+        for flight in Flight.objects.all():
+            for field, expected_verbose_name in self.fields_and_verbose_names.items():
+                real_verbose_name = flight._meta.get_field(field).verbose_name
+
+                self.assertEqual(real_verbose_name, expected_verbose_name)
+
+    def test_default_values(self):
+        """Test default parameter for fields of Flight instances"""
+
+        for flight in Flight.objects.all():
+            for field, expected_default_value in self.fields_and_default_values.items():
+                real_default_value = flight._meta.get_field(field).default
+
+                self.assertEqual(real_default_value, expected_default_value)
+
+    def test_instance_string_display(self):
+        """Test string display of Flight instance"""
+
+        for flight in Flight.objects.all():
+            real_string_display = str(flight)
+            expected_string_display = str(flight.route) + ' - ' \
+                + str(flight.departure_time)
+
+            self.assertEqual(real_string_display, expected_string_display)
+
+    def test_model_verbose_name(self):
+        """Test verbose_name of Flight model"""
+
+        real_model_verbose_name = Flight._meta.verbose_name.title()
+
+        self.assertEqual(real_model_verbose_name, self.model_verbose_name)
+
+    def test_model_verbose_name_plural(self):
+        """Test verbose_name_plural of Flight model"""
+
+        real_model_verbose_name_plural = \
+            Flight._meta.verbose_name_plural.title()
+
+        self.assertEqual(
+            real_model_verbose_name_plural,
+            self.model_verbose_name_plural
+        )
+
+    def test_model_ordering(self):
+        """Test ordering of Flight model"""
+
+        real_model_ordering = Flight._meta.ordering
 
         self.assertEqual(
             real_model_ordering,
